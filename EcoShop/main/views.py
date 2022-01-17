3e8cb485695a6cc500016352657886e2d6e5e499
Zakharov-A <global user.email redzed427@gmail.com>
2022-01-17 08:13:13 +0300

    Django_less_7

--- EcoShop/main/views.py
+++ EcoShop/main/views.py
@@ -4,5 +4,7 @@ def index(request):
     return render(request, 'main/index.html')
 def about(request):
     return render(request, 'main/about.html')
+def contacts(request):
+    return render(request, 'main/contacts.html')
 
 
