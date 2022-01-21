e2f7d1031720968ab1d7c3af3f572b339c094db6
Zakharov-A <global user.email redzed427@gmail.com>
2022-01-21 07:25:33 +0300

    Django_less_11_finish

--- EcoShop/news/views.py
+++ EcoShop/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Artiles.objects.order_by('date')
@@ -12,6 +12,18 @@ class NewsDetailView(DetailView):
     model = Artiles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Artiles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Artiles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
 
 
 def create(request):
